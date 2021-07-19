68fd22d65c183a1d6d1d029dca1186c028945880
Liam Pugh <78647249+Shutt90@users.noreply.github.com>
2021-07-19 14:01:26 +0100

    1hr6mins in

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const Person = require("./person");
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
-const person1 = new Person("John Doe", 30);
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('<h1>Homepage</h1>')
+    }
 
-person1.greeting();
+})
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
